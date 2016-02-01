84391dcec97b11061a1429e1f2692bc2b52522e6
TullyHanson <btullyha@uci.edu>
2016-01-31 22:04:37 -0800

    Modified method Rotate90DegreeCW to have two versions: Mobile & Desktop

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -55,7 +55,8 @@ public class Tile : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        StartCoroutine(Rotate90DegreeCW());
+        // Add a listener for input from the player
+        StartCoroutine(Rotate90DegreeCW_Desktop());
     }
 
     // Shift the tiles position up graphically
@@ -77,44 +78,77 @@ public class Tile : MonoBehaviour {
         yield return null;
     }
 
-
-    IEnumerator Rotate90DegreeCW()
+    // Listens for player input, uses mobile input functions 
+    IEnumerator Rotate90DegreeCW_Mobile()
     {
-    // MOBILE //     for (int i = 0; i < Input.touchCount; i++)
-    // MOBILE //     {
-    // MOBILE //         Touch thisTouch = Input.GetTouch(i);
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch thisTouch = Input.GetTouch(i);
 
-            // MOBILE // if (thisTouch.phase == TouchPhase.Began)
-            if(Input.GetMouseButtonDown(0))
+            // If the screen was tapped and this tile isn't already waiting to be deleted
+            if (thisTouch.phase == TouchPhase.Began && !waitingToBeDeleted)
             {
-                if (!waitingToBeDeleted)
+                // Finds the position on the screen that was tapped
+                Vector3 rawPosition = cam.ScreenToWorldPoint(thisTouch.rawPosition);
+                Vector3 targetPosition = new Vector3(rawPosition.x, rawPosition.y, 0.0f);
+
+                // If the tapped point lies within the bounds of this tile
+                if (Mathf.Abs(targetPosition.x - transform.position.x) < halfTileWidth && Mathf.Abs(targetPosition.y - transform.position.y) < halfTileWidth)
                 {
-                    // MOBILE // Vector3 rawPosition = cam.ScreenToWorldPoint(thisTouch.rawPosition);
-                    Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
-                    Vector3 targetPosition = new Vector3(rawPosition.x, rawPosition.y, 0.0f);
+                    // Play rotation sound
+                    GetComponent<AudioSource>().Play();
+                    // Rotate 90 degrees clockwise
+                    Vector3 oldRotation = transform.eulerAngles;
+                    transform.eulerAngles = new Vector3(0.0f, 0.0f, oldRotation.z - 90.0f);
 
-                    if (Mathf.Abs(targetPosition.x - transform.position.x) < halfTileWidth && Mathf.Abs(targetPosition.y - transform.position.y) < halfTileWidth)
-                    {
-                        GetComponent<AudioSource>().Play();
-                        Vector3 oldRotation = transform.eulerAngles;
-                        transform.eulerAngles = new Vector3(0.0f, 0.0f, oldRotation.z - 90.0f);
-
-                        rotateColorArray();
-
-                        StartCoroutine(CheckAdjacent());
-                    }
+                    //Rotate the color array of this tile to match its new orientation
+                    rotateColorArray();
+                    // Check tiles adjacent to this tile to see if there are color matches
+                    StartCoroutine(CheckAdjacent());
                 }
             }
-        // MOBILE // }
+        }
         yield return null;
     }
 
+    /*
+    Listens for player input, uses desktop input functions
+    Also works for mobile input as well.
+    */
+    IEnumerator Rotate90DegreeCW_Desktop()
+    {
+        // If the screen was clicked and this tile isn't already waiting to be deleted
+        if (Input.GetMouseButtonDown(0) && !waitingToBeDeleted)
+        {
+            // Finds the position on the screen that was clicked
+            Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 targetPosition = new Vector3(rawPosition.x, rawPosition.y, 0.0f);
+
+            // If the clicked point lies within the bounds of this tile
+            if (Mathf.Abs(targetPosition.x - transform.position.x) < halfTileWidth && Mathf.Abs(targetPosition.y - transform.position.y) < halfTileWidth)
+            {
+                // Play rotation sound
+                GetComponent<AudioSource>().Play();
+                // Rotate 90 degrees clockwise
+                Vector3 oldRotation = transform.eulerAngles;
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, oldRotation.z - 90.0f);
+                
+                //Rotate the color array of this tile to match its new orientation
+                rotateColorArray();
+                // Check tiles adjacent to this tile to see if there are color matches
+                StartCoroutine(CheckAdjacent());
+            } 
+        }
+        yield return null;
+    }
+
+    // Checks the 4 adjacent positions around this Tile to look for color matches
     IEnumerator CheckAdjacent()
     {
-        // Order the adjacency array in { Above, Right, Below, Left }
-        // Simplifies the for loop
+        // Order the adjacency array in { Above, Right, Below, Left } to simplify the later loop
         Tile[] adjacentTiles = { null, null, null, null };
 
+        // Fill tile array when the given positions are within the bounds of the game grid
         if (arrayPosX >= 0 && arrayPosY <= Grid.gameGrid.gridHeight - 2)
             adjacentTiles[0] = Grid.gameGrid.tileArray[arrayPosX, arrayPosY + 1];
         if (arrayPosX <= Grid.gameGrid.gridWidth - 2 && arrayPosY >= 0)
@@ -124,36 +158,55 @@ public class Tile : MonoBehaviour {
         if (arrayPosX >= 1 && arrayPosY >= 0)
             adjacentTiles[3] = Grid.gameGrid.tileArray[arrayPosX - 1, arrayPosY];
         
-        
         ArrayList pointsToCheckForFall = new ArrayList();
         ArrayList tilesToDestroy = new ArrayList();
         bool atleastOneDeleted = false;
 
         for (int i = 0; i < adjacentTiles.Length; i++)
         {
+            // If there is an adjacent tile at this index (Comes into play at edges/corners/upper layer of tiles)
             if(adjacentTiles[i] != null)
             {
+                /*
+                Determines if the appropriate opposite inner sides of 2 adjacent tiles have the same color
+
+                Example, given that we are tile X:
+                _0_
+                1X2
+                _3_
+                
+                When checking to see if there is a match with:
+                    Tile 0, we want to check the top color of X with the bottom of 0.
+                    Tile 1, we want to check the left color of X with the right of 1.
+                    Tile 2, we want to check the right color of X with the left of 2.
+                    Tile 3, we want to check the bottom color of X with the top of 3.
+                */
                 if (colorArray[i] == adjacentTiles[i].colorArray[(i + 2) % 4])
                 {
+                    // An adjacent tile is set to be deleted, so we know we want to delete the middle tile later
                     atleastOneDeleted = true;
+                    // Set this boolean value so future taps don't move this tile out of position
                     waitingToBeDeleted = true;
 
+                    // Check to make sure we don't try to delete the same tile twice
                     if (!adjacentTiles[i].waitingToBeDeleted)
                     {
                         adjacentTiles[i].waitingToBeDeleted = true;
+                        // Add the position of this tile to an array so we know to notify tiles above it to fall
                         pointsToCheckForFall.Add(new Vector2(adjacentTiles[i].arrayPosX, adjacentTiles[i].arrayPosY + 1));
+                        // Add this tile to a list to be deleted
                         tilesToDestroy.Add(adjacentTiles[i]);
                     }
                 }
             }
         }
 
+        // There was a match, so this tile must be deleted as well
         if (atleastOneDeleted)
         {
             pointsToCheckForFall.Add(new Vector2(arrayPosX, arrayPosY + 1));
-
             tilesToDestroy.Add(this);
-            
+            // Notify the grid to update tile positions accordingly
             Grid.gameGrid.UpdateGameGrid(pointsToCheckForFall, tilesToDestroy);
         }
         yield return null;
@@ -161,30 +214,31 @@ public class Tile : MonoBehaviour {
 
     public IEnumerator Fall()
     {
+        // Wait until this tile is finished shifting upwards
         while (isShiftingUp)
             yield return new WaitForEndOfFrame();
 
+        // Increment the semaphore to prevent further tile creation until this tile is done falling
         Grid.fallInProgressInt++;
         bool keepDropping = arrayPosY >= 2 ? true : false;
 
-        // While this tile has room below to fall or the block directly below me is falling as well
+        // While this tile has room below to fall || the block directly below it is falling as well
         while (keepDropping && (Grid.gameGrid.tileArray[arrayPosX, arrayPosY - 1] == null || Grid.gameGrid.tileArray[arrayPosX, arrayPosY - 1].isFalling))
         {
             isFalling = true;
 
-            //Fall
+            // Find the desired location for this tile to fall to
             Vector3 target = new Vector3(transform.position.x, transform.position.y - tileWidth, transform.position.z);
             float speed = 2.5f;
-            print("Start falling: " + this.GetInstanceID());
+            // Move its position slowly to make it look fluid
             while (transform.position.y > target.y)
             {
-                print(transform.position.y + " > " + target.y);
                 float step = speed * Time.deltaTime;
                 transform.position = Vector3.MoveTowards(transform.position, target, step);
                 yield return new WaitForEndOfFrame();
             }
-            print("Finished: " + transform.position.y);
             arrayPosY = arrayPosY - 1;
+            // Change its position in the tile array since it finished falling
             Grid.gameGrid.tileArray[arrayPosX, arrayPosY] = this;
             Grid.gameGrid.tileArray[arrayPosX, arrayPosY + 1] = null;
             
@@ -193,14 +247,17 @@ public class Tile : MonoBehaviour {
             
             yield return new WaitForEndOfFrame();
         }
+        // Decrement the semaphore to allow more tiles to be created
         Grid.fallInProgressInt--;
         yield return null;
     }
 
+    // Rotates the color array so it lines up with the colors represented on screen
     public void rotateColorArray()
     {
-        //{ Top, Right, Bottom, Left }
+        //{Top, Right, Bottom, Left}
         int placeHolder = colorArray[3];
+        // Slides whole array forward one index, wrapping around
         for(int i = colorArray.Length - 1; i > 0; i--)
             colorArray[i] = colorArray[i - 1];
         colorArray[0] = placeHolder;
